8c5d3c9d2ba00c10bf3a8e0316a6871745f49253
Szpakusik <szpakusik@gmail.com>
2021-02-10 11:38:28 +0100

    Use env in services

--- catalogjs/src/app/product.service.ts
+++ catalogjs/src/app/product.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product, Data } from './product'
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators'
+import { tap } from 'rxjs/operators';
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +12,12 @@ export class ProductService {
 
   constructor(private http: HttpClient ) { }
 
-  private productsURL: string = "https://gorest.co.in/public-api/products";
   private pageCount: number = 1;
   private pageLimit: number = 1;
   public productsArray: Product[] = [];
 
   getProducts(): void{
-    this.http.get<Data>(this.productsURL+`?page=${this.pageCount}`, { observe:"body" }).pipe(
+    this.http.get<Data>(environment.apiUrl+`/products?page=${this.pageCount}`, { observe:"body" }).pipe(
       tap( (res) => { this._fetchNextPage(res) } )
     )
     .subscribe( (res) => {
