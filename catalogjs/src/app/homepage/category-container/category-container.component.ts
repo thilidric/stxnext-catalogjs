30937d31495aa06e0fb3879aa248cac8085fbf5c
Szpakusik <szpakusik@gmail.com>
2021-02-10 14:07:38 +0100

    Category loop / loader + provide data

--- catalogjs/src/app/homepage/category-container/category-container.component.ts
+++ catalogjs/src/app/homepage/category-container/category-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/category.service';
 
 @Component({
   selector: 'app-category-container',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor( public categoryService: CategoryService ) { }
 
   ngOnInit(): void {
   }
--- catalogjs/src/app/homepage/category-container/category/category.component.ts
+++ catalogjs/src/app/homepage/category-container/category/category.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Category } from 'src/app/category';
 
 @Component({
   selector: 'app-category',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.sass']
 })
 export class CategoryComponent implements OnInit {
+
+  @Input() category!: Category;
 
   constructor() { }
 
