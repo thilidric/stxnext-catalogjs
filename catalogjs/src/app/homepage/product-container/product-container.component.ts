06fce1a68b960524a6cc1bcc2b2623fd6d8e04f4
Szpakusik <szpakusik@gmail.com>
2021-02-10 15:27:24 +0100

    Display product list

--- catalogjs/src/app/homepage/product-container/product-container.component.ts
+++ catalogjs/src/app/homepage/product-container/product-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/product.service';
 
 @Component({
   selector: 'app-product-container',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor( public productService: ProductService ) { }
 
   ngOnInit(): void {
   }
