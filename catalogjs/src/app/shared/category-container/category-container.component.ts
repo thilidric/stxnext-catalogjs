d46a3f840aadeba7bf851d803ff0b37f690473dd
Szpakusik <szpakusik@gmail.com>
2021-02-16 14:41:18 +0100

    Make use of api.service

--- catalogjs/src/app/shared/category-container/category-container.component.ts
+++ catalogjs/src/app/shared/category-container/category-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/interfaces/category';
 import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
@@ -10,8 +11,23 @@ export class CategoryContainerComponent implements OnInit {
 
   constructor( public categoryService: CategoryService ) { }
 
+  public categoriesArrayShuffled: Category[] = [];
+  public hasFetched = false;
+
   ngOnInit(): void {
     this.categoryService.getCategories()
+      .subscribe( (res) => {
+        this.categoriesArrayShuffled = res.slice();
+        this.shuffleArray(this.categoriesArrayShuffled);
+        this.hasFetched = true;
+      })
+  }
+
+  shuffleArray(array: Category[]) :void{
+    for ( let i = array.length - 1; i > 0; i-- ) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
   }
 
 }
